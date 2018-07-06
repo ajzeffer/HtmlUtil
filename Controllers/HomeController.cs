db2d11cce33ef2e2524f44a561f6729f8187efbb
Adam Zeffer <az@prodiginet.com>
2018-07-06 11:44:18 -0700

    init: util to unescape html

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TransStateUnescapeApp.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace TransStateUnescapeApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string json)
+        {
+            try{
+            var jsonResult = JObject.Parse(UnescapeHtml(json));
+            return Json(jsonResult);
+            }
+            catch(Exception ex){
+                return  Json(ex);
+            }
+        }
+
+        public string UnescapeHtml(string escapedHtml)
+        {
+            var result = escapedHtml
+                            .Replace("&a;", "&")
+                            .Replace("&q;", "\"")
+                            .Replace("&l;", "<")
+                            .Replace("&g;", ">");
+            return result;
+        }
+    }
+}
